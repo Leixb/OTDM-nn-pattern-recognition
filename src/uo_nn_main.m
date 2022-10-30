782537ac9d78413798a4eba81557b776a123f42a
LeixB <abone9999@gmail.com>
2022-10-30 22:10:15 +0100

    Finish document

--- src/uo_nn_main.m
+++ src/uo_nn_main.m
@@ -12,7 +12,7 @@ te_seed    = 26060125;
 %
 % Parameters for optimization
 %
-la = 0.0;                                                     % L2 regularization.
+la = 0.1;                                                     % L2 regularization.
 epsG = 10^-6; kmax = 10000;                                   % Stopping criterium.
 ils=3; ialmax = 2; kmaxBLS=30; epsal=10^-3;c1=0.01; c2=0.45;  % Linesearch.
 isd = 7; icg = 2; irc = 2 ; nu = 1.0;                         % Search direction.
@@ -22,6 +22,8 @@ sg_emax = kmax; sg_ebest = floor(0.01*sg_emax);               % SGM stopping con
 % Optimization
 %
 
+tr_p = 20000; te_q = tr_p/10; tr_freq = 0.0;  
+
 t1=clock;
 [Xtr,ytr,wo,fo,tr_acc,Xte,yte,te_acc,niter,tex]=uo_nn_solve(num_target,tr_freq,tr_seed,tr_p,te_seed,te_q,la,epsG,kmax,ils,ialmax,kmaxBLS,epsal,c1,c2,isd,sg_al0,sg_be,sg_ga,sg_emax,sg_ebest,sg_seed,icg,irc,nu);
 t2=clock;
@@ -29,5 +31,6 @@ fprintf(' wall time = %6.1d s.\n', etime(t2,t1));
 
 
 
-
-uo_nn_Xyplot(Xte, yte, wo)
+%yte_plt = yte(:,1:250);
+%Xte_plt = Xte(:,1:250);
+%uo_nn_Xyplot(Xte_plt, yte_plt, wo)
